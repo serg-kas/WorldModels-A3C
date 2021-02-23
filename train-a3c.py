d34ef9edd09e9fd0d93c453494ec5ef5ae7fb805
serg-kas <serg_kas@mail.ru>
2021-02-23 21:18:16 +0300

    rollback

--- train-a3c.py
+++ train-a3c.py
@@ -80,7 +80,6 @@ def train_process(global_agent, vae, rnn, update_term, pid, state_dims, hidden_d
     running_means = []
     step = 0
     for ep in range(max_ep):
-        print('Train process, ep=', ep)  # 23-02-21
         obs = env.reset()
         score = 0.
         i = 0
@@ -189,7 +188,6 @@ def test_process(global_agent, vae, rnn, update_term, pid, state_dims, hidden_di
     worse = 0
     best_agent_state = None
     for ep in range(test_ep):
-        print('Test process, ep=',ep) #23-02-21
         agent.load_state_dict(global_agent.state_dict())
         env.reset()
         score = 0.
